d35cb78ef72859bdd4c340380d3652392869993c
John Koelndorfer <john@johnk.io>
2024-02-04 10:26:43 -0600

    d2types: Export Diablo2Item

--- src/d2lfg/d2core/d2types/__init__.py
+++ src/d2lfg/d2core/d2types/__init__.py
@@ -6,12 +6,15 @@ This module contains code to model Diablo 2 engine types.
 """
 
 from .bodyloc import Diablo2BodyLoc, Diablo2BodyLocs
+from .item import Diablo2Item, Diablo2ItemType
 from .playerclass import Diablo2PlayerClass, Diablo2PlayerClasses
 from .skill import Diablo2Skill
 
 __all__ = [
     "Diablo2BodyLoc",
     "Diablo2BodyLocs",
+    "Diablo2Item",
+    "Diablo2ItemType",
     "Diablo2PlayerClass",
     "Diablo2PlayerClasses",
     "Diablo2Skill",
